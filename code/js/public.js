ffca7f8c17e53be300f7f9012443052704df1ce6
pcxys <pcxiaoyaoshu@gmail.com>
2022-12-26 23:27:04 +0800

    update
    View details CHANGELOG.md

---
+++ code/js/public.js
@@ -0,0 +1,6 @@
+function exit() {
+	alert("您已成功退出");
+}
+function UnregisteredReminder() {
+	alert ("未注册，无法进行查看");
+}
---
+++ code/js/user.js
@@ -0,0 +1,12 @@
+function PersonalInformation() {
+	alert("跳转到个人信息设置");
+}
+function MatePreference() {
+	alert("跳转到择偶偏好设置");
+}
+function PersonalData() {
+	alert("跳转到个人数据设置");
+}
+function SystemSettings() {
+	alert("跳转到系统设置设置");
+}
