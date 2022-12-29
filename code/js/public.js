d8535f42ce46a3817c1a53fd0340b8684c7eaec2
pcxys <pcxiaoyaoshu@gmail.com>
2022-12-29 22:37:13 +0800

    update
    View details CHANGELOG.md

--- code/js/public.js
+++ code/js/public.js
@@ -1,3 +1,6 @@
+function load() {
+	alert(7 + 2);
+}
 function exit() {
 	alert("您已成功退出");
 }
@@ -7,5 +10,7 @@ function UnregisteredReminder() {
 // 点击右上角用户名，更改用户名，并在控制台输出。
 function Changeusername(xing,ming) {
 	document.getElementById("username").innerHTML= xing + ming;
+	document.getElementById("username").style.color= "blue";
+	document.getElementById("exit").style.visibility= "hidden";
 	console.log("已更改用户名");
 }
