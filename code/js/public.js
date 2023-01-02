acc6968f9cf49819791a95aede741697d782cdc7
pcxys <pcxiaoyaoshu@gmail.com>
2023-01-02 14:30:29 +0800

    update
    View details CHANGELOG.md

--- code/js/public.js
+++ code/js/public.js
@@ -1,9 +1,46 @@
 function $(id){
 	return document.getElementById(id); 
 }
-function indexRead() {
-	//alert ("成功调取");
+function basic() {
 	get();
+	//navlist();
+}
+function navlist() {
+	//1.获取xhr
+	var xhr = createXhr();
+	//2.创建请求，使用get方式，异步请求
+	xhr.open("get","data/navlist.json",true);
+	//3.回调函数
+	xhr.onreadystatechange = function(){
+		// console.log(xhr.readyState + "," + xhr.status);
+		if(xhr.readyState==4&&xhr.status==200){
+			var resText = xhr.responseText;
+			//将resText转换成js对象格式
+			var persons = JSON.parse(resText);
+			// 输出读取到的数据到控制台
+			// console.log(persons);
+			console.log (persons.length);
+			// 遍历导航菜单项，并生成html语句进行显示
+			for (j = 0; j<persons.length;j++) {
+				let html = "";
+				html += '<li class="nav-item"><a class="nav-link" href="'+ persons[j].url +'" onclick = "onNavlist(this.innerText)">'+ persons[j].name +'</a></li>';
+				console.log (html);
+				$("nav").innerHTML += html;
+			}
+		}
+	}
+	//4.发送请求
+	xhr.send();
+}
+function onNavlist(navName) {
+	if (navName == "主页") {
+		window.location.href = "main.html";
+	} else if (navName == "会员广场") {
+		window.location.href = "membersquare.html";
+	}else if (navName == "个人设置") {
+		window.location.href = "user.html";
+	}
+	console.log("载入的网址是:" + navName);
 }
 function createXhr() {
 	var xhr;
@@ -19,7 +56,7 @@ function get() {
 	//1.获取xhr
 	var xhr = createXhr();
 	//2.创建请求，使用get方式，异步请求
-	xhr.open("get","avatar.json",true);
+	xhr.open("get","data/avatar.json",true);
 	//3.回调函数
 	xhr.onreadystatechange = function(){
 		// console.log(xhr.readyState + "," + xhr.status);
@@ -37,16 +74,7 @@ function get() {
 				for (k = 0; k<rows[j].cells.length; k++)
 				{
 					let update = "";
-					update += "<img";
-					update += " ";
-					update += "src= '";
-					update += persons[number].url;
-					update += "'";
-					update += " ";
-					update += "class = 'img'";
-					update += " ";
-					update += ">";
-					update += "</img>";
+					update += "<img src = '" + persons[number].url + "' class = 'img' ></img>";
 					// console.log (update);
 					if (number<19) {
 						number++;
